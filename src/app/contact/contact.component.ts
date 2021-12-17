a60eb4e28faed6319c87feb2be26dc2bcfdf8cb6
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-12-17 19:09:58 +0000

    reactive forms part1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -34,6 +34,7 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
